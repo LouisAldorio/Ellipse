8ddfb400b05c8b820041ce9decb1f0676242b057
louisaldorio <louisaldorio@gmail.com>
2020-11-09 11:46:44 +0700

    second commit

--- Ellipse.go
+++ Ellipse.go
@@ -4,12 +4,8 @@ import (
 	"math"
 )
 
-// if center is 0,0
-type Init struct {
-	A, B float64
+// Get Eccentricity of Ellipse
+func GetEccentricity(A,B float64) float64 {
+ 	return (math.Sqrt(math.Pow(A, 2) - math.Pow(B, 2))) / A
 }
 
-// Get Eccentricity of Ellipse
-func (e *Init) GetEccentricity() float64 {
-	return (math.Sqrt(math.Pow(e.A, 2) - math.Pow(e.B, 2))) / e.A
-}
